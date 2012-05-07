59e20e3e660cd12f6caae44aeb1706943333b791
Jen Bourey <jennifer.bourey@gmail.com>
2012-05-07 13:15:42 -0700

    Beginning conference app customization

--- Resources/js/config.js
+++ Resources/js/config.js
@@ -30,8 +30,8 @@ Titanium.App.Properties.setString('locale', 'en_US');
 // Base url of the portal, which should be of the format
 // http[s]://server[:port][/context]. This URL is *not* expected to contain a
 // trailing slash.
-exports.BASE_PORTAL_URL = 'https://dev-uportal-showcase.unicon.net';
-exports.PORTAL_CONTEXT = '/uPortal';
+exports.BASE_PORTAL_URL = 'https://umobile.unicon.net';
+exports.PORTAL_CONTEXT = '';
 exports.LAYOUT_URL = exports.BASE_PORTAL_URL + exports.PORTAL_CONTEXT + '/layout.json';
 exports.LAYOUT_VIEW = constants.layoutTypes['GRID_LAYOUT'];
 
@@ -39,7 +39,7 @@ exports.LAYOUT_VIEW = constants.layoutTypes['GRID_LAYOUT'];
 Ti.App.Properties.setInt('SERVER_SESSION_TIMEOUT', parseInt((2 * 60 * 60 * 1000), 10));
 
 //References value of LoginProxy.loginMethods constant.
-exports.LOGIN_METHOD = "Cas";
+exports.LOGIN_METHOD = "LocalLogin";
 
 exports.CAS_URL = exports.BASE_PORTAL_URL + '/cas';
 exports.SHIB_URL = exports.BASE_PORTAL_URL + "/Shibboleth.sso/Login?target=" + exports.BASE_PORTAL_URL + exports.PORTAL_CONTEXT + "/Login";
